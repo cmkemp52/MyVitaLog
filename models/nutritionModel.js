e90f0bb3b87d34e69ac19048b810e32a325f031b
Christopher Kemp <cmkemp52@gmail.com>
2019-10-24 14:53:11 -0400

    added api call for food data

---
+++ models/nutritionModel.js
@@ -0,0 +1,7 @@
+const axios = require('axios')
+
+async function foodData(input){
+    const response = await axios.get(`https://api.nal.usda.gov/fdc/v1/${input}?api_key=oIbSq6bzsa0JDZ9pQFbwu5r3VOckChzNCjbz2JiN`)
+    return response;
+}
+module.exports = foodData;
