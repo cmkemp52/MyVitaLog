d4401e70595b998dc4b397b7b208f4291032ae54
Christopher Kemp <cmkemp52@gmail.com>
2019-10-28 22:18:36 -0400

    now pulls 3 pages of results from USDA

--- models/searchModel.js
+++ models/searchModel.js
@@ -1,12 +1,32 @@
 const axios = require('axios')
 
 async function foodsearch(input, page = 1){
+    let results2 = false;
+    let results3 = false;
     const response = await axios.post('https://api.nal.usda.gov/fdc/v1/search?api_key=oIbSq6bzsa0JDZ9pQFbwu5r3VOckChzNCjbz2JiN', {
-        generalSearchInput:`${input}`,
-        requireAllWords: "true",
-        page:`${page}`
-    })
-    return response.data.foods;
+            generalSearchInput:`${input}`,
+            requireAllWords: "true",
+            pageNumber:`${page}`
+            
+        })
+    const results1 = response.data.foods;
+    if(response.data.currentPage<response.data.totalPages){
+        const response2 = await axios.post('https://api.nal.usda.gov/fdc/v1/search?api_key=oIbSq6bzsa0JDZ9pQFbwu5r3VOckChzNCjbz2JiN', {
+            generalSearchInput:`${input}`,
+            requireAllWords: "true",
+            pageNumber:`${page+1}`
+        })
+        results2 = response2.data.foods;
+        if(response2.data.currentPage<response2.data.totalPages){
+            const response3 = await axios.post('https://api.nal.usda.gov/fdc/v1/search?api_key=oIbSq6bzsa0JDZ9pQFbwu5r3VOckChzNCjbz2JiN', {
+                generalSearchInput:`${input}`,
+                requireAllWords: "true",
+                pageNumber:`${page+2}`
+            })
+            results3 = response3.data.foods;
+        }
+    }
+    return {results1, results2, results3};
 
 }
 module.exports = foodsearch;
